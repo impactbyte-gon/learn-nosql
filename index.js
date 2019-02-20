34278ac3dee15b46708f1ac520aa659f16a057f5
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-20 17:06:33 +0700

    Implement Task model

--- index.js
+++ index.js
@@ -16,10 +16,19 @@ mongoose.connect(`${process.env.URL}/${process.env.DB_NAME}`, {
   useNewUrlParser: true
 })
 
+// User model => users collection
 const User = mongoose.model('User', {
   name: String,
   age: Number,
-  email: String
+  email: String,
+  birthDate: Date
+})
+
+// Task model => tasks collection
+const Task = mongoose.model('Task', {
+  text: String,
+  completed: Boolean,
+  date: Date
 })
 
 // -----------------------------------------------------------------------------
@@ -34,6 +43,13 @@ app.get('/users', async (req, res) => {
   res.send({
     message: 'List of all users',
     users: await User.find()
+  })
+})
+
+app.get('/tasks', async (req, res) => {
+  res.send({
+    message: 'List of all tasks',
+    tasks: await Task.find()
   })
 })
 
